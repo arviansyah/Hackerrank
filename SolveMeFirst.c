49349a9ff6e0cf05f78146a7fd194b386e8ce0ad
Admiral Budi Arviansyah <arvi.admiral@gmail.com>
2019-01-07 23:47:53 +0700

    1st problem

---
+++ SolveMeFirst.c
@@ -0,0 +1,18 @@
+#include<algorithm>
+#include<iostream>
+#include<cmath>
+
+int solvemefirst(int a, int b)
+{
+    return a+b;
+}
+
+Int main ()
+{
+    int Num1, Num2 ;
+    int Sum ;
+    cin>>Num1>>Num2;
+    Sum = solvemefirst(Num1,Num2);
+    cout<<Sum;
+    return 0 ;
+}
