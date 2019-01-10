a10a683775a6ab60aaf829e152232780c9fad79b
Admiral Budi Arviansyah <arvi.admiral@gmail.com>
2019-01-10 21:33:05 +0700

    Add files via upload
    3rd problem

---
+++ BasicDataType.c
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+
+    int I;
+    long L;
+    char C;
+    float F;
+    double D;
+
+    scanf("%d %ld %c %f %lf",&I,&L,&C,&F,&D);
+
+    printf("%d \n%ld \n%c \n%f \n%lf \n",I,L,C,F,D);
+
+
+    return 0;
+}
+
